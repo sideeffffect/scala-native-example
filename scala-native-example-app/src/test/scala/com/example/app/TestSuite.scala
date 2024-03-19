27b0f3d9c95b88debb64244aeb6f6caef80fa6a6
Ondra Pelech <ondra.pelech@gmail.com>
2024-03-19 01:01:17 +0100

    Example app Scala 3 syntax

--- scala-native-example-app/src/test/scala/com/example/app/TestSuite.scala
+++ scala-native-example-app/src/test/scala/com/example/app/TestSuite.scala
@@ -2,10 +2,8 @@ package com.example.app
 
 import com.example.library.Adder
 
-class TestSuite extends munit.FunSuite {
-  test("hello") {
+class TestSuite extends munit.FunSuite:
+  test("hello"):
     val adder = new Adder(1)
     val result = adder.add(2)
     assertEquals(result, 42)
-  }
-}
